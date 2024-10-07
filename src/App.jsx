0aa88ca57678a00b44110e8b46ec91b3108817aa
mr_X <mr.Aleksandrsmith@gmail.com>
2024-10-07 15:06:16 +0300

    finish routers section

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useEffect, useState } from "react";
-import Product from "./pages/Product";
-import Homepage from "./pages/Homepage";
-import Pricing from "./pages/Pricing";
-import PageNotFound from "./pages/PageNotFound";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AppLayout from "./pages/AppLayout";
-import "./index.css";
-import Login from "./pages/Login";
-import CityList from "./components/CityList";
+import { useEffect, useState } from "react"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 
-const BASE_URL = "http://localhost:8000";
+import Product from "./pages/Product"
+import Homepage from "./pages/Homepage"
+import Pricing from "./pages/Pricing"
+import PageNotFound from "./pages/PageNotFound"
+import AppLayout from "./pages/AppLayout"
+import "./index.css"
+import Login from "./pages/Login"
+import CityList from "./components/CityList"
+import CountriesList from "./components/CountriesList"
+import City from "./components/City"
+import Form from "./components/Form"
+
+const BASE_URL = "http://localhost:8000"
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [cities, setCities] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(function () {
     async function fetchCities() {
       try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
+        setIsLoading(true)
+        const res = await fetch(`${BASE_URL}/cities`)
+        const data = await res.json()
+        setCities(data)
       } catch {
-        alert("Some error happens during fetching data..");
+        alert("Some error happens during fetching data..")
       } finally {
-        setIsLoading(false);
+        setIsLoading(false)
       }
     }
-    fetchCities();
-  }, []);
+    fetchCities()
+  }, [])
 
   return (
     <BrowserRouter>
@@ -39,21 +43,22 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries</p>} />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountriesList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,23 @@
+import { useNavigate, useSearchParams } from "react-router-dom"
 import styles from "./Map.module.css"
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>
+  const navigate = useNavigate()
+
+  const [searchParams, setSearchParams] = useSearchParams()
+  const lat = searchParams.get("lat")
+  const lng = searchParams.get("lng")
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>Map</h1>
+      <h4>
+        Position: {lat} & {lng}
+      </h4>
+      <button onClick={() => setSearchParams({ lat: 12, lng: 9 })}>
+        click
+      </button>
+    </div>
+  )
 }
 
 export default Map
